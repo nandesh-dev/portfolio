e197497ddee9010a472026705c6b98001750626d
Aoi <nandesh.dev@gmail.com>
2023-06-28 22:10:41 +0530

    FIXed import paths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -r docs
 mkdir docs
 
-parcel build src/desktop/index.html --dist-dir docs/desktop
-parcel build src/loader/index.html --dist-dir docs/
+parcel build src/desktop/index.html --dist-dir docs/desktop --public-url "https://www.nandesh.com/desktop/"
+parcel build src/loader/index.html --dist-dir docs/ --public-url "https://www.nandesh.com/"
