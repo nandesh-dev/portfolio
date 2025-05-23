b695e38b0ea0c6c1465cbc4c438f80e41767e936
nandesh-dev <nandesh.dev@gmail.com>
2025-05-23 09:12:47 +0000

    Optimize object shader code for older hardware
    - Replace all pow() functions to faster rough approximations
    - Reduce the number of variables declared
    - Refactor into multiple functions for better variable scopping

--- web/materials/object/fragment.glsl
+++ web/materials/object/fragment.glsl
@@ -9,28 +9,41 @@ uniform float uSunIntensity;
 varying vec3 vPosition;
 varying vec3 vNormal;
 
+float specularFactor(vec3 lightDirection, vec3 viewDirection) {
+  float s = (1.0 - uRoughness) * dot(vNormal, normalize(lightDirection + viewDirection)) + uRoughness;
+  return s * s;
+}
+
+float lightAngleFactor(vec3 lightDirection) {
+  float a = dot(vNormal, lightDirection) * 0.5 + 0.5;
+  return a / (0.3 * a + 0.7);
+}
+
+float lightDistanceFactor(float lightDistance) {
+  return 1.0 / (0.00000001 + lightDistance * lightDistance);
+}
+
+float fogFactor(float viewDistance) {
+    float f = dot(
+      vec3(step(0.0, vPosition.y) * step(0.0, uCameraPosition.y), step(vPosition.y, 0.0) * step(uCameraPosition.y, 0.0), step(uCameraPosition.y * vPosition.y, 0.0)), 
+      vec3(0.0, 1.0, abs(min(vPosition.y, uCameraPosition.y) / (vPosition.y - uCameraPosition.y)))
+    ) * viewDistance;
+
+    return clamp(1.0 - 1.0 / (1.0 + 0.75 * f + 0.2 * f * f + 0.1 * f * f * f * f), 0.0, 1.0);
+}
+
 void main() {
-    float lightIntensity = uSunIntensity;
     float lightDistance = length(uSunPosition - vPosition);
     vec3 lightDirection = normalize(uSunPosition - vPosition);
 
     vec3 viewDirection = normalize(uCameraPosition - vPosition);
     float viewDistance = length(uCameraPosition - vPosition);
 
-    vec3 halfVector = normalize(lightDirection + viewDirection);
-
-    float specularFactor = pow(uRoughness + (1.0 - uRoughness) * dot(vNormal, halfVector), 2.0);
-    float lightAngleFactor = pow(dot(vNormal, lightDirection), 0.8);
-    float lightDistanceFactor = pow(max(lightDistance, 0.00000001), -2.0);
-
-    float depth = max(-vPosition.y, 0.0);
-    float cameraAltitude = max(uCameraPosition.y, 0.0);
-    float fogDistance = viewDistance * (depth / max(depth + cameraAltitude, 0.00000001));
-    float fogFactor = clamp(1.0 - pow(1.8, -max(fogDistance * 1.6, 0.00000001)), 0.0, 1.0);
-
-    float lightness = specularFactor * lightAngleFactor * lightDistanceFactor * lightIntensity;
-
-    vec3 color = mix(mix(uObjectColorDark, uObjectColorLight, lightness), uFogColor, fogFactor);
+    vec3 color = mix(
+      mix(uObjectColorDark, uObjectColorLight, specularFactor(lightDirection, viewDirection) * lightAngleFactor(lightDirection) * lightDistanceFactor(lightDistance) * uSunIntensity),
+      uFogColor,
+      fogFactor(viewDistance)
+    );
 
     gl_FragColor = vec4(color, 1.0);
 }
