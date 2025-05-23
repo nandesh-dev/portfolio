3d27e1b0205c2c16f4db744861387dd525a0cb8b
nandesh-dev <nandesh.dev@gmail.com>
2025-05-23 09:29:29 +0000

    Optimse sky shader code for older hardware
    - Replace pow() with faster rough approximation
    - Reduce the number of variables

--- web/materials/sky/fragment.glsl
+++ web/materials/sky/fragment.glsl
@@ -4,18 +4,10 @@ uniform vec3 uColorDark;
 varying vec3 vPosition;
 
 void main() {
-    float scale = 0.1;
-    float altitude = max(vPosition.y, 0.0) * scale;
-    float depth = max(-vPosition.y, 0.0) * scale;
-
-    float factor = 0.0;
-    if (vPosition.y < 0.0) {
-        factor = 1.0 - clamp(depth * 4.0, 0.0, 1.0);
-    } else {
-        factor = clamp(pow(2.0, -altitude), 0.0, 1.0);
-    }
-
-    vec3 color = mix(uColorDark, uColorLight, factor);
+    vec3 color = mix(
+      uColorDark, uColorLight, 
+      clamp(mix(vPosition.y * 0.4 + 1.0, 1.0 / (1.0 + 0.004 * vPosition.y * vPosition.y), step(0.0, vPosition.y)), 0.0, 1.0)
+    );
 
     gl_FragColor = vec4(color, 1.0);
 }
